d85476119b212e52e482f4a5399b348360020052
aoraptor <aowongster@gmail.com>
2014-12-26 21:46:39 -0800

    adding public and helper static file function

--- helloworld.js
+++ helloworld.js
@@ -1,19 +1,38 @@
 var http = require('http');
+var fs = require('fs');
+
+function serveStaticFile(res, path, contentType, responseCode){
+  if(!responseCode)
+    responseCode = 200;
+  fs.readFile(__dirname + path, function(err,data){
+    if(err){
+      res.writeHead(500, {'Content-Type': 'text/plain' });
+      res.end('500 - Internal Error');
+    }
+    else {
+      res.writeHead(responseCode, {'Content-Type': contentType});
+      res.end(data);
+    }
+  });
+};
 
 http.createServer(function(req,res){
   var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
   switch(path) {
     case '':
-      res.writeHead(200, {'Content-Type': 'text/plain' });
-      res.end('Homepage');
+      serveStaticFile(res, '/public/home.html', 'text/html');
       break;
     case '/about':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('About');
+      serveStaticFile(res, '/public/about.html', 'text/html');
+      break;
+    case '/img/logo.jpg':
+      serveStaticFile(res, '/public/img/logo.jpg', 'image/jpeg');
+      break;
+    case '/img/kitty.gif':
+      serveStaticFile(res, '/public/img/kitty.gif', 'image/gif');
       break;
     default:
-      res.writeHead(404, {'Content-Type': 'text/plain' });
-      res.end('Not Found');
+      serveStaticFile(res, '/public/notfound.html', 'text/html', 404);
       break;
   }
 }).listen(8088);
